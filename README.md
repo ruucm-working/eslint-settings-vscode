d4222a57964b0036995f4db0a299bcfecd8fdf60
ruucm <ruucm.a@gmail.com>
2020-09-12 21:56:06 +0900

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+## eslint-settings-vscode (WIP)
+
+An Example node project with ESlint settings for VS Code auto save
---
+++ Test.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export function Test() {
+  return <div>Test</div>
+}
+
+Test.defaultProps = {}
