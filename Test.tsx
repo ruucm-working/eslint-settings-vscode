bde1123e86e10e9be0e655b4aa38ee902a13fed9
ruucm <ruucm.a@gmail.com>
2020-09-13 16:07:12 +0900

    update

--- Test.tsx
+++ Test.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react'
 
 export function Test() {
-  return <div>Test</div>
+  return (
+    <div>
+      Test
+      <div />
+    </div>
+  )
 }
+
+let hey
 
 Test.defaultProps = {}
